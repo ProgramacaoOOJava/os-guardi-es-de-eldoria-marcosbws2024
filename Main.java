a74fd51f1dfbfbce3bf683644b70866bea773e4c
marcosbws2024 <marcosbws2024@gmail.com>
2026-07-14 04:07:38 +0000

    Desafio Novato

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        //instanciando os objetos da classe Personagem
+        Personagem heroi1 = new Personagem("Aragorn", "Guerreiro", 10, 100, 50.0);
+        Personagem heroi2= new  Personagem("Gandalf", "Mago", 20, 80, 100.0);
 
-        .
-        .
-        .
-
+        //chamando o metodo exibirStatus() para exibir as informações dos personagens
+        heroi1.exibirStatus();
+        System.out.println();
+        heroi2.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,30 @@
+/* DESAFIO  NOVATO*/
 public class Personagem {
+//Declarando os atributos da classe Personagem
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
+    //contrutor da classe Personagem
+    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double podeBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = podeBase;
     }
 
+    /* Metodo para Exibir as informações */
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
     }
+
 }
 
