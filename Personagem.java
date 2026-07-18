4c325b858b085557edf0c0fbb2f385c4938d2441
marcosbws2024 <marcosbws2024@gmail.com>
2026-07-18 07:19:26 +0000

    Personagem - Desafio Aventureiro

--- Personagem.java
+++ Personagem.java
@@ -1,13 +1,15 @@
-/* DESAFIO  NOVATO*/
-public class Personagem {
-//Declarando os atributos da classe Personagem
+//Desafio Aventureiro
+
+//Classe Abstrata Personagem, que será a superclasse para as classes Guerreiro e Mago
+public abstract class Personagem {
+//variáveis de instância
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
-    //contrutor da classe Personagem
+//CONSTRUTOR DA CLASSE PERSONAGEM
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
         this.classe = classe;
@@ -16,7 +18,7 @@ public class Personagem {
         this.poderBase = poderBase;
     }
 
-    /* Metodo para Exibir as informações */
+    //Metodo para Exibir as informações
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
         System.out.println("Nome: " + nome);
@@ -25,6 +27,7 @@ public class Personagem {
         System.out.println("Pontos de Vida: " + pontosDeVida);
         System.out.println("Poder Base: " + poderBase);
     }
-
+    
+    //método abstrato que será implementado nas subclasses
+    public abstract void usarHabilidadeEspecial(); 
 }
-
