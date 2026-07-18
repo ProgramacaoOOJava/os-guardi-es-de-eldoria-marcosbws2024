f12a0efcbf84c7aa462e77a6c412e6c3103ddaad
marcosbws2024 <marcosbws2024@gmail.com>
2026-07-18 07:22:04 +0000

    Classe Guerreiro - Desafio Aventureiro

---
+++ Guerreiro.java
@@ -0,0 +1,21 @@
+//Deasafio Aventureiro
+
+//Classe Guerreiro que herda da classe Personagem
+public class Guerreiro extends Personagem {
+    private double forca;
+//Construtor da classe Guerreiro
+    public Guerreiro(String nome,
+            String classe,
+            int nivel,
+            int pontosDeVida,
+            double poderBase, double forca) {
+//Chamando o construtor da superclasse Personagem
+        super(nome, classe, nivel, pontosDeVida, poderBase);
+        this.forca = forca;
+    }
+//Implementação do método abstrato da superclasse Personagem
+    public void usarHabilidadeEspecial() {
+        System.out.println("Guerreiro " + this.nome + " ataca com Espada Flamejante!");
+    }
+
+}
