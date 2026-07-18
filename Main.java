4f68535a022002b6a12c58b622cdd8f70100955d
marcosbws2024 <marcosbws2024@gmail.com>
2026-07-18 04:10:11 -0300

    Refactor character instantiation and add polymorphism demo
    Updated the character instantiation to include specific subclasses and added polymorphism demonstration with an ArrayList of characters.

--- Main.java
+++ Main.java
@@ -1,12 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        //instanciando os objetos da classe Personagem
-        Personagem heroi1 = new Personagem("Aragorn", "Guerreiro", 10, 100, 50.0);
-        Personagem heroi2= new  Personagem("Gandalf", "Mago", 20, 80, 100.0);
+    // Instanciando o Guerreiro e o Mago
+    Guerreiro guerreiro = new Guerreiro("Arthus", "Guerreiro", 10, 100, 50.0, 20.0);
+    Mago mago = new Mago("Elenara", "Mago", 12, 80, 60.0, 30.0);
+    // Exibindo os status dos personagens
+    guerreiro.exibirStatus();
+    mago.exibirStatus();
+    System.out.println("************************Ataques dos Personagens:*******************");
+    guerreiro.usarHabilidadeEspecial();
+    mago.usarHabilidadeEspecial();
+    // Criando o arrayList de Personagem para demonstrar o polimorfismo
+    System.out.println("************************Demonstração de polimorfismo:*******************");
+    List<Personagem> herois = new ArrayList<>();
+    // adicionando os personagens ao arrayList
+    herois.add(guerreiro);
+    herois.add(mago);
+    // Criando novos Personagens e adicionando ao arrayList
+    herois.add(new Guerreiro("Leon", "Guerreiro", 8, 90, 45.0, 15.0));
+    herois.add(new Mago("Merlin", "Mago", 15, 70, 70.0, 40.0));
 
-        //chamando o metodo exibirStatus() para exibir as informações dos personagens
-        heroi1.exibirStatus();
-        System.out.println();
-        heroi2.exibirStatus();
+    // Exibindo os status e habilidades especiais de todos os personagens no
+    // arrayList
+    for (Personagem heroi : herois) {
+      heroi.exibirStatus();
+      heroi.usarHabilidadeEspecial();
+      System.out.println("---------------------------------------------------");
+    }
     }
 }
